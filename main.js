ca272a759f48f1231729a2d63c28850449d4518e
kareem333j <87768872+kareem333j@users.noreply.github.com>
2023-03-04 01:55:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function side_on(){
     document.getElementById("zapayn").style = "opacity: 0.5";
     document.getElementById("footer").style = "opacity: 0.5";
     document.getElementById("hr1").style = "opacity: 0.5";
-    document.getElementById("hr2").style = "opacity: 0.5";
+    document.getElementById("hr2").style = "opacity: 0.5; background:#18191A; width:100%";
 }
 
 function side_of(){
@@ -12,6 +12,6 @@ function side_of(){
     document.getElementById("section2").style = "opacity: 1";
     document.getElementById("zapayn").style = "opacity: 1";
     document.getElementById("footer").style = "opacity: 1";
-    document.getElementById("hr1").style = "opacity: 1";
-    document.getElementById("hr2").style = "opacity: 1";
+    document.getElementById("hr1").style = "opacity: 1; background:#18191A; width:100%";
+    document.getElementById("hr2").style = "opacity: 1; background:#18191A; width:100%";
 }
